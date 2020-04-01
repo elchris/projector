71ac31b06c9575757cd1293f5f045506e904de98
elchris <frenchy@gmail.com>
2020-04-01 03:17:50 -0500

    updated data sets and improved formatting of the JSON output

--- src/Projection.php
+++ src/Projection.php
@@ -17,16 +17,26 @@ class Projection
      * @var float
      */
     public $deltaIncrementPercentage;
+    /**
+     * @var string
+     */
+    public $summary;
 
     public function __construct(float $total, float $delta, float $deltaIncrementPercentage)
     {
         $this->total = $total;
         $this->delta = $delta;
         $this->deltaIncrementPercentage = $deltaIncrementPercentage;
+        $this->summary =
+            number_format($this->total)
+            .' - '.number_format($this->delta)
+            .' - '.number_format(($this->deltaIncrementPercentage * 100), 2).'%';
     }
 
     public function __toString()
     {
-        return number_format($this->total).': '.number_format($this->delta).': '.number_format($this->deltaIncrementPercentage,4)."\n";
+        return
+            $this->summary
+            ."\n";
     }
 }
